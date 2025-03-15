a62aeddafd673bb238fffe3d61fab240ee8b727d
Chin3ll <Ochinell@outlook.com>
2025-03-15 23:25:31 +0100

    removed mysql db

--- feedback_sorting/settings.py
+++ feedback_sorting/settings.py
@@ -74,19 +74,11 @@ WSGI_APPLICATION = 'feedback_sorting.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-        "default": {
-            "ENGINE": "mysql.connector.django",
-            "NAME": "feedbackgenerator",
-            "USER": "root",
-            "PASSWORD": "",
-            "HOST": "localhost",
-            "PORT": "3306",
-            "OPTIONS": {
-                "autocommit": True,
-            },
-        }
-    }
-
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
